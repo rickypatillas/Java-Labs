b4da7b2917ea800b3c8170132d16d9ea16d24b3d
Ricky Roque <raroque29@hotmail.co>
2017-10-09 16:42:06 -0500

    part 5

--- part_05/Exercise_01.java
+++ part_05/Exercise_01.java
@@ -9,3 +9,93 @@ package part_05;
  * the varying state of each private variable. You may name these classes whatever you like.
  */
 
+class Class_01 {
+    public static void main(String[] args) {
+        Class_02 box = new Class_02(); //my object
+        box.setAlpha(150);
+        System.out.println("value of alpha is: " + box.getAlpha());
+
+        box.setBeta(95);
+        System.out.println("your grade is " + box.getBeta());
+
+        box.setGamma(88);
+        System.out.println("your grade is " + box.getGamma());
+
+        Class_03 nihongo = new Class_03();
+        System.out.println(nihongo.getPio());
+        nihongo.setPio(6);
+        System.out.println(nihongo.getPio());
+
+        System.out.println(box.myMeth()); ;
+
+    }
+}
+
+
+    class Class_02 {
+
+        private int alpha;
+        private int beta;
+        private int gamma;
+
+
+        public int getAlpha() {
+            return alpha;
+        }
+
+        public void setAlpha(int alpha) {
+            if (alpha > 0 && alpha <= 100) {
+                this.alpha = alpha;
+            } else {
+                this.alpha = -1;
+            }
+        }
+
+        public int getBeta() {
+            return beta;
+        }
+
+        public void setBeta(int beta) {
+            if (beta < 100 && beta >= 90) {
+                System.out.println("you get an A-");
+            }
+            this.beta = beta;
+        }
+
+        public int getGamma() {
+            return gamma;
+        }
+
+        public void setGamma(int gamma) {
+            if (gamma < 90 && gamma >= 80) {
+                System.out.println("you get a B+");
+            }
+            this.gamma = gamma;
+        }
+
+        public int myMeth() {
+            int r = 5;
+            return r;
+        }
+    }
+
+    class Class_03 {
+        private int pio;
+
+        public int getPio() {
+            for (int i = 1; i < 8; i++) {
+                if (i == 5) {
+                    System.out.println("value is 5");
+                    continue;
+                }
+                System.out.println("value is not 5, value is " + i);
+            }
+            return pio;
+        }
+
+        public void setPio(int pio) {
+            this.pio = pio;
+        }
+
+    }
+
